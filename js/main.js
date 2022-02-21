bd1743a6c39f07b9e2fda005e55a8a2b7e0170ac
Jakub Karbowski <karbo360@gmail.com>
2022-02-21 15:47:11 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const burgerBtn = document.querySelector('.hamburger')
+const navMobile = document.querySelector('.nav-mobile')
+
+const clicker = () => {
+   
+   if( burgerBtn.classList.toggle('is-active')){
+    navMobile.style.transform = 'translateX(0)'
+    }
+    else{
+        navMobile.style.transform = 'translateX(-300px)'
+    }
+}
+document.addEventListener('DOMContentLoaded', function(){
+    const nav = document.querySelector('.nav-desktop')
+    function addShadow(){
+        if(window.scrollY >= 100){
+            
+            nav.classList.add('active')
+
+        }else{
+            nav.classList.remove('active')
+        }
+
+    }
+
+    window.addEventListener('scroll',addShadow)
+})
+document.addEventListener('DOMContentLoaded', function(){
+    const nav = document.querySelector('.nav-hamburger')
+    function addShadow(){
+        if(window.scrollY >= 100){
+            
+            nav.classList.add('active')
+
+        }else{
+            nav.classList.remove('active')
+        }
+
+    }
+
+    window.addEventListener('scroll',addShadow)
+})
+
+
+burgerBtn.addEventListener('click',clicker);
